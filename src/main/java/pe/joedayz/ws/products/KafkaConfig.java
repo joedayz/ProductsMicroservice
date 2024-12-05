9c3dc5eebe97b6e5608c052245240a60b43a9619
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2024-12-04 22:10:11 -0500

    creando topico kafka

---
+++ src/main/java/pe/joedayz/ws/products/KafkaConfig.java
@@ -0,0 +1,23 @@
+package pe.joedayz.ws.products;
+
+import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+/**
+ * @author josediaz
+ **/
+@Configuration
+public class KafkaConfig {
+
+  @Bean
+  NewTopic createTopic() {
+    return TopicBuilder.name("product-created-events-topic")
+        .partitions(3)
+        .replicas(3)
+        .configs(Map.of("min.insync.replicas","2"))
+        .build();
+  }
+}
